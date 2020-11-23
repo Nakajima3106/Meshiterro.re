62dbda786eba66f41133561e03ad75353cfb1eac
Nakajima3106 <uver777.333@gmail.com>
2020-11-23 14:30:50 +0000

    画像投稿機能作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '370ac554c9ed9d76701c84cfdf15bdb0078aa861c0f80cce5a1c90561563580a36284c964adcb3f7349ea5e45caa032b4f7d15fdf1ddb5fba0b3f02720b0acb7'
